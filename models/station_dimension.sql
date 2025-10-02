5bf0613503960f4d57962e7df29977a9e66cbba2
raushan.kumar1712@gmail.com <raushan.kumar1712@gmail.com>
2025-10-02 13:32:42 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,14 @@
+{{config(materialized= 'table')}}
+
+with BIKE AS (
+select
+start_statio_id as station_id,
+start_station_name as station_name,
+start_lat as station_lat,
+start_lng as station_lng
+from {{ source('demo', 'bike') }}
+
+)
+select
+*
+from bike
